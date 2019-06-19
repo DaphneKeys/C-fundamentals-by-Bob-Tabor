b323f69c7f652cb9de7cbbfbdbc747bafdb317af
Daphne-JKC <jiekeipeggy@yahoo.com>
2019-06-19 20:27:05 +0800

    Add files via upload

---
+++ UnderstandingArrays.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnderstandingArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            int[] number = new int[5];
+
+            number[0] = 4;
+            number[1] = 8;
+            number[2] = 15;
+            number[3] = 16;
+            number[4] = 23;
+            //number[5] = 42; 
+
+            //Console.WriteLine(number[1]);
+            Console.WriteLine(number.Length);
+            Console.ReadLine();
+            */
+
+
+            //int[] numbers = new int[] { 4, 8, 15, 16, 23, 42 }; 
+
+            //string[] names = new string[] { "Eddie", "Alex", "Michael", "David Lee" };
+
+            /*
+            for (int i = 0; i < names.Length; i++) //prints all strings in the array 
+            {
+                Console.WriteLine(names[i]);
+            }
+            Console.ReadLine();
+            */
+
+            /*
+            foreach (string name in names) //allows us to read through the string of names 
+            {
+                Console.WriteLine(name); 
+            }
+            Console.ReadLine();
+            */
+
+            string zig = "You can get what you want out of life " +
+                "if you help enough other people get what they want.";
+
+            char[] charArray = zig.ToCharArray(); //long string split into characters 
+            Array.Reverse(charArray); //reverse the char 
+
+            foreach (char zigChar in charArray)
+            {
+                Console.Write(zigChar);
+            }
+            Console.ReadLine(); 
+
+
+
+        }
+
+    }
+}
---
+++ Variables.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            int x;
+            int y;
+            x = 7;
+            y = x + 3;
+
+            Console.WriteLine(y);
+            Console.ReadLine(); 
+            */
+
+            Console.WriteLine("What is your name?");
+            Console.Write("Type your first name: ");
+            string myFirstName;
+            myFirstName = Console.ReadLine();
+
+            //string myLastName;
+            //Console.Write("Type your last name: ");
+            //myLastName = Console.ReadLine();
+
+            Console.WriteLine("Type your last name: ");
+            string myLastName = Console.ReadLine(); 
+
+            Console.WriteLine("Hello, " + myFirstName + " " + myLastName);
+            Console.ReadLine(); 
+        }
+    }
+}

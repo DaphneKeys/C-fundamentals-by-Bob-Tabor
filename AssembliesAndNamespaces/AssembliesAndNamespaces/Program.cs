5c41bf2c815c33c6c4ff080d57a34c9796425118
Daphne-JKC <jiekeipeggy@yahoo.com>
2019-06-23 16:08:32 +0800

    Add files via upload

---
+++ AssembliesAndNamespaces/AssembliesAndNamespaces/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using System.Net;
+
+namespace AssembliesAndNamespaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //WebClient download String Method / download html string
+            WebClient client = new WebClient();
+            string reply = client.DownloadString("http://msdn.microsoft.com");
+
+            Console.WriteLine(reply);
+            //Write to a text file
+            File.WriteAllText(@"C:\delicious\WriteText.txt", reply); //replace your location
+            Console.ReadLine(); 
+        }
+    }
+}

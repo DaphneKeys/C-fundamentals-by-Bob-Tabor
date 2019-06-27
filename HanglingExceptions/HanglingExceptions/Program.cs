a8634429e5f3399b43a49add4d2d40962600571e
Daphne-JKC <jiekeipeggy@yahoo.com>
2019-06-27 21:11:04 +0800

    Add files via upload

---
+++ HanglingExceptions/HanglingExceptions/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HanglingExceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string content = File.ReadAllText(@"C:\delicious\Exampl.txt");
+                Console.WriteLine(content);
+                
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine("There was a problem!");
+                Console.WriteLine("Make sure the name of the file is named correct. e.g Example.txt");
+            }
+            catch (DirectoryNotFoundException ex)
+            {
+                Console.WriteLine("There was a problem!");
+                Console.WriteLine(@"Make sure the directory C:\delicious exists.");
+            }
+            catch (Exception ex) //general
+            {
+                Console.WriteLine("There was a problem!");
+                Console.WriteLine(ex.Message); 
+            }
+            finally
+            {
+                //Code to finalize
+                //Setting objects to null
+                //Closing database connections
+                Console.WriteLine("Closing application now...");
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
